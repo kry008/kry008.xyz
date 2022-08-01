a42a9afb935454407ce673c14f94a7b77deaf097
kry008 <pkrzysaaaa@gmail.com>
2022-08-01 19:17:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# [kry008.ml](https://kry008.ml)
+
+Aby przeczytać więcej o projekcie, zobacz [wpis na blogu](https://kry008.ml/blog/?p=71)
---
+++ script.js
@@ -0,0 +1,19 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  window.onscroll = function () { window.scrollTo(0, 0); };
+  function f1() {
+      navigator.clipboard.writeText('k̠̠̄r̲̱̱ȳ̲͞0̲̳̿0̠̳͞8̠̳̳#8914').catch(function(err) {
+          document.getElementById("info").style.color = "rgb(255, 0, 0)";
+          console.log('Failed to copy text: ', err);
+          return setTimeout(() => {
+              document.getElementById("info").innerHTML = "";
+              document.getElementById("info").style.color = "rgb(9, 255, 0)";
+          }, 1000);
+      });
+      document.getElementById("info").innerHTML = "Copied username to clipboard";
+      setTimeout(() => {
+          document.getElementById("info").innerHTML = "";
+          document.getElementById("info").style.color = "rgb(9, 255, 0)";
+      }, 1000);
+  }
